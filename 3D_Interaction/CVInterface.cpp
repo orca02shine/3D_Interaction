42747c639138d3d9822cbf1f87183b4187ffe239
orca02shine <orca02shinebite18@gmail.com>
2025-01-25 16:46:02 +0900

    aa

--- 3D_Interaction/CVInterface.cpp
+++ 3D_Interaction/CVInterface.cpp
@@ -435,7 +435,7 @@ void CVInterface::MakeContour(cv::Mat &img) {
 	cv::findContours(gray, Contours, Hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_TC89_L1);
 
 	for (auto& con : Contours) {
-		double approxDist = 0.003*cv::arcLength(con, true);//ãﬂéóê∏ìx
+		double approxDist = 0.004*cv::arcLength(con, true);//ãﬂéóê∏ìx
 		cv::approxPolyDP(con, con, approxDist, true);
 	}
 
